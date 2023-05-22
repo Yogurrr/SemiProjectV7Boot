4390803229458bb14b3145df9337f045345563e7
LeeSeungYeon <masakisqq@naver.com>
2023-05-22 10:14:43 +0900

    쿼리 메서드를 이용한 페이징 정렬 처리

--- src/main/java/yogurrr/springboot/semiprojectv7/dao/BoardDAOImpl.java
+++ src/main/java/yogurrr/springboot/semiprojectv7/dao/BoardDAOImpl.java
@@ -3,6 +3,7 @@ package yogurrr.springboot.semiprojectv7.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import yogurrr.springboot.semiprojectv7.model.Board;
 import yogurrr.springboot.semiprojectv7.repository.BoardRepository;
@@ -18,7 +19,9 @@ public class BoardDAOImpl implements BoardDAO{
 
     @Override
     public List<Board> selectBoard(int cpage) {
-        Pageable paging = PageRequest.of(cpage, 25);
+        // 페이징 시 저열 순서 지정
+        Pageable paging = PageRequest.of(cpage, 25, Sort.by("bno").descending());
+        PageRequest.of(cpage, 25, Sort.Direction.DESC, "bno");
 
         return boardRepository.findAll(paging).getContent();
     }
