9d76a9ef12a3af51f1f24d2abb15a549ef07b6cc
LeeSeungYeon <masakisqq@naver.com>
2023-05-24 17:10:37 +0900

    자료실 - PdsAttach 모델 구현

---
+++ src/main/java/yogurrr/springboot/semiprojectv7/model/PdsAttach.java
@@ -0,0 +1,28 @@
+package yogurrr.springboot.semiprojectv7.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pdsattach")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsAttach {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pano;
+
+    private String fname;
+    private String ftype;
+    private String fsize;
+
+    @Column(insertable = false, updatable = false)
+    private String fdowns;
+
+    private Integer pno;
+}
