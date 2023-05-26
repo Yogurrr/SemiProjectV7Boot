dd04f1464d633b77b50496f3e9a2c0a2ad2a60c3
LeeSeungYeon <masakisqq@naver.com>
2023-05-26 12:13:20 +0900

    자료실 댓글 - 본문보기 댓글 repository 구현

---
+++ src/main/java/yogurrr/springboot/semiprojectv7/repository/PdsReplyRepository.java
@@ -0,0 +1,14 @@
+package yogurrr.springboot.semiprojectv7.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import yogurrr.springboot.semiprojectv7.model.PdsReply;
+
+import java.util.List;
+
+public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
+
+    // @Query("from PdsReply where pno = :pno order by refno asc")
+    List<PdsReply> findByPnoOrderByRefnoAscRegdateAsc(int pno);
+}
