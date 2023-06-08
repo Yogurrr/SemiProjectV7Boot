84c5b898b07d6988245599417eb98dfc02e1ee65
LeeSeungYeon <masakisqq@naver.com>
2023-06-08 11:52:47 +0900

    갤러리 - Gallery List DAO

--- src/main/java/yogurrr/springboot/semiprojectv7/dao/GalleryDAOImpl.java
+++ src/main/java/yogurrr/springboot/semiprojectv7/dao/GalleryDAOImpl.java
@@ -1,11 +1,19 @@
 package yogurrr.springboot.semiprojectv7.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import yogurrr.springboot.semiprojectv7.model.Board;
 import yogurrr.springboot.semiprojectv7.model.GalAttach;
 import yogurrr.springboot.semiprojectv7.model.Gallery;
 import yogurrr.springboot.semiprojectv7.repository.GalleryRepository;
 import yogurrr.springboot.semiprojectv7.repository.GalleryaRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository("galdao")
 public class GalleryDAOImpl implements GalleryDAO{
@@ -28,5 +36,18 @@ public class GalleryDAOImpl implements GalleryDAO{
     @Override
     public int insertGalAttach(GalAttach ga) {
         return Math.toIntExact(galarepo.save(ga).getGano());
+    }
+
+    @Override
+    public Map<String, Object> selectGallery(Integer cpg) {
+        // 페이징 시 저열 순서 지정
+        Pageable paging = PageRequest.of(cpg, 25, Sort.by("gno").descending());
+
+        Map<String, Object> gals = new HashMap<>();
+        // Gallery와 GalAttach를 조인해서 리스트로 가져옴
+        gals.put("gallist", galrepo.findAllBy(paging).getContent());
+        gals.put("cntpg", galrepo.findAllBy(paging).getTotalPages());
+
+        return gals;
     }
 }
