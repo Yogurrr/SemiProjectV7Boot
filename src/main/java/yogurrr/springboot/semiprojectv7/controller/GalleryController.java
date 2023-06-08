2084ea20d631d1bb6b664a0b82f18608bba5df8a
LeeSeungYeon <masakisqq@naver.com>
2023-06-08 13:18:08 +0900

    갤러리 - Gallery View Controller

--- src/main/java/yogurrr/springboot/semiprojectv7/controller/GalleryController.java
+++ src/main/java/yogurrr/springboot/semiprojectv7/controller/GalleryController.java
@@ -58,4 +58,13 @@ public class GalleryController {
 
         return viewPage;
     }
+
+    @GetMapping("/view")
+    public ModelAndView view(int gno) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("gal", galsrv.readOneGallery(gno));
+        mv.setViewName("gallery/view");
+
+        return mv;
+    }
 }
